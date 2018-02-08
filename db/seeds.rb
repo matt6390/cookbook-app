0e9001d27faad790e2709ba35178ceeb4151feae
matt6390 <matthewstone639@gmail.com>
2018-02-07 21:28:57 -0600

    Created an HTML page to demo the index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.create(name: "Cheap")
+Category.create(name: "Junk Food")
+Category.create(name: "Edible")
+
+
+CategoryRecipe.create!([
+                        {category_id: 1, recipe_id: 1},
+                        {category_id: 2, recipe_id: 2}
+                        ])
+
